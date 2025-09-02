47e206361a57e63b43ef179e0468c622ffdb31ad
luoyonglun1 <vengroth3322@gmail.com>
2025-09-02 19:05:22 +0700

    Update cardData.js

--- src/product/cardData.js
+++ src/product/cardData.js
@@ -3,7 +3,7 @@ const productData = [
     {
         id : 1 ,
         img : "https://havenwearpakistan.com/cdn/shop/files/elevation-oversized-t-shirt-668531.jpg?v=1721757196&width=1946" ,
-        product : "airpord air",
+        product : "WOMAN SHIRT",
         price : "199$",
         text : "With plenty of talk and listen time, voice-activated Siri access",
         category : "Woman" ,
@@ -12,7 +12,7 @@ const productData = [
     {
         id : 2 ,
          img : "https://m.media-amazon.com/images/I/51ce76AJ-WL.jpg" ,
-        product : "airpord air",
+        product : "WOMAN SHIRT",
         price : "199$",
         text : "With plenty of talk and listen time, voice-activated Siri access",
         category : "Woman" ,
@@ -21,7 +21,7 @@ const productData = [
     {
         id : 3 ,
         img : "https://shop.vogue.co.uk/cdn/shop/products/Vogue_T-Shirt_black_1.jpg?v=1684164309&width=1080" ,
-        product : "airpord air",
+        product : "WOMAN SHIRT",
         price : "199$",
         text : "With plenty of talk and listen time, voice-activated Siri access",
         category : "Woman" ,
@@ -30,7 +30,7 @@ const productData = [
     {
         id : 4 ,
           img : "https://voguetella.store/cdn/shop/files/Vogueprintedblack.png?v=1726688299&width=550" ,
-        product : "airpord air",
+        product : "WOMAN SHIRT",
         price : "199$",
         text : "With plenty of talk and listen time, voice-activated Siri access",
         category : "Woman" ,
@@ -39,7 +39,7 @@ const productData = [
     {
         id : 5 ,
        img : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRAaTaFxw1BBBqx4CZ5g1TZ6owxcGkaWsX3Y0BUMjWplnWLQL65" ,
-        product : "airpord air",
+        product : "WOMAN SHIRT",
         price : "199$",
         text : "With plenty of talk and listen time, voice-activated Siri access",
         category : "Woman" ,
@@ -48,7 +48,7 @@ const productData = [
     {
         id : 6 ,
          img : "https://thebanyantee.com/cdn/shop/files/Black-T-shirt.jpg?v=1721380366" ,
-        product : "airpord air",
+        product : "MAN SHIRT",
         price : "199$",
         text : "With plenty of talk and listen time, voice-activated Siri access",
         category : "Man" ,
@@ -57,7 +57,7 @@ const productData = [
     {
         id : 7 ,
         img : "https://files.cdn.printful.com/o/upload/bfl-image/f5/10333_l_collage%20vintage%20design%20.jpg" ,
-        product : "airpord air",
+        product : "MAN SHIRT",
         price : "199$",
         text : "With plenty of talk and listen time, voice-activated Siri access",
         category : "Man" ,
@@ -66,7 +66,7 @@ const productData = [
     {
         id : 8 ,
         img : "https://i.pinimg.com/736x/d7/d1/4e/d7d14e0168681e8aacdb7115a3a9b9f6.jpg" ,
-        product : "airpord air",
+        product : "MAN SHIRTr",
         price : "199$",
         text : "With plenty of talk and listen time, voice-activated Siri access",
         category : "Man" ,
@@ -75,7 +75,7 @@ const productData = [
     {
         id : 9 ,
         img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu91lqkHpDCEp3L5GsbCsgVzomm3fo_My89w&s" ,
-        product : "airpord air",
+        product : "MAN SHIRT",
         price : "199$",
         text : "With plenty of talk and listen time, voice-activated Siri access",
         category : "Man" ,
@@ -84,7 +84,7 @@ const productData = [
     {
         id : 10 ,
         img : "https://pitshirts.in/cdn/shop/files/InShot-20241209_155704713.jpg?v=1755319102&width=1946" ,
-        product : "airpord air",
+        product : "MAN SHIRT",
         price : "199$",
         text : "With plenty of talk and listen time, voice-activated Siri access",
         category : "Man" ,
@@ -93,7 +93,7 @@ const productData = [
     {
         id : 11 ,
         img : "https://i.etsystatic.com/21891580/r/il/e46f30/4093954139/il_1588xN.4093954139_joi1.jpg" ,
-        product : "airpord air",
+        product : "KID SHIRT",
         price : "199$",
         text : "With plenty of talk and listen time, voice-activated Siri access",
         category : "Kid" ,
@@ -102,7 +102,7 @@ const productData = [
     {
         id : 12 ,
         img : "https://i.etsystatic.com/21891580/r/il/7108fe/4083553949/il_1588xN.4083553949_tqhq.jpg" ,
-        product : "airpord air",
+        product : "KID SHIRT",
         price : "199$",
         text : "With plenty of talk and listen time, voice-activated Siri access",
         category : "Kid" ,
@@ -111,7 +111,7 @@ const productData = [
     {
         id : 13 ,
         img : "https://i.etsystatic.com/21891580/r/il/01d7c0/4108945039/il_1588xN.4108945039_l4pr.jpg" ,
-        product : "airpord air",
+        product : "KID SHIRT",
         price : "199$",
         text : "With plenty of talk and listen time, voice-activated Siri access",
         category : "Kid" ,
@@ -120,7 +120,7 @@ const productData = [
     {
         id : 14 ,
         img : "https://m.media-amazon.com/images/I/51ce76AJ-WL.jpg" ,
-        product : "airpord air",
+        product : "KID SHIRT",
         price : "199$",
         text : "With plenty of talk and listen time, voice-activated Siri access",
         category : "Kid" ,
@@ -129,7 +129,7 @@ const productData = [
     {
         id : 15 ,
        img : "https://shop.vogue.co.uk/cdn/shop/products/Vogue_T-Shirt_black_1.jpg?v=1684164309&width=1080" ,
-        product : "airpord air",
+        product : "KID SHIRT",
         price : "199$",
         text : "With plenty of talk and listen time, voice-activated Siri access",
         category : "Kid" ,
