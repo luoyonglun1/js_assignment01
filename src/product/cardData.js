89f2024aec1ab91019c6a776d3906e82b1ae402d
luoyonglun1 <vengroth3322@gmail.com>
2025-09-02 15:38:13 +0700

    first commit

---
+++ src/product/cardData.js
@@ -0,0 +1,140 @@
+const productData = [
+
+    {
+        id : 1 ,
+        img : "https://havenwearpakistan.com/cdn/shop/files/elevation-oversized-t-shirt-668531.jpg?v=1721757196&width=1946" ,
+        product : "airpord air",
+        price : "199$",
+        text : "With plenty of talk and listen time, voice-activated Siri access",
+        category : "Woman" ,
+
+    },
+    {
+        id : 2 ,
+         img : "https://m.media-amazon.com/images/I/51ce76AJ-WL.jpg" ,
+        product : "airpord air",
+        price : "199$",
+        text : "With plenty of talk and listen time, voice-activated Siri access",
+        category : "Woman" ,
+
+    },
+    {
+        id : 3 ,
+        img : "https://shop.vogue.co.uk/cdn/shop/products/Vogue_T-Shirt_black_1.jpg?v=1684164309&width=1080" ,
+        product : "airpord air",
+        price : "199$",
+        text : "With plenty of talk and listen time, voice-activated Siri access",
+        category : "Woman" ,
+
+    },
+    {
+        id : 4 ,
+          img : "https://voguetella.store/cdn/shop/files/Vogueprintedblack.png?v=1726688299&width=550" ,
+        product : "airpord air",
+        price : "199$",
+        text : "With plenty of talk and listen time, voice-activated Siri access",
+        category : "Woman" ,
+
+    },
+    {
+        id : 5 ,
+       img : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRAaTaFxw1BBBqx4CZ5g1TZ6owxcGkaWsX3Y0BUMjWplnWLQL65" ,
+        product : "airpord air",
+        price : "199$",
+        text : "With plenty of talk and listen time, voice-activated Siri access",
+        category : "Woman" ,
+
+    },
+    {
+        id : 6 ,
+         img : "https://thebanyantee.com/cdn/shop/files/Black-T-shirt.jpg?v=1721380366" ,
+        product : "airpord air",
+        price : "199$",
+        text : "With plenty of talk and listen time, voice-activated Siri access",
+        category : "Man" ,
+
+    },
+    {
+        id : 7 ,
+        img : "https://files.cdn.printful.com/o/upload/bfl-image/f5/10333_l_collage%20vintage%20design%20.jpg" ,
+        product : "airpord air",
+        price : "199$",
+        text : "With plenty of talk and listen time, voice-activated Siri access",
+        category : "Man" ,
+
+    },
+    {
+        id : 8 ,
+        img : "https://i.pinimg.com/736x/d7/d1/4e/d7d14e0168681e8aacdb7115a3a9b9f6.jpg" ,
+        product : "airpord air",
+        price : "199$",
+        text : "With plenty of talk and listen time, voice-activated Siri access",
+        category : "Man" ,
+
+    },
+    {
+        id : 9 ,
+        img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu91lqkHpDCEp3L5GsbCsgVzomm3fo_My89w&s" ,
+        product : "airpord air",
+        price : "199$",
+        text : "With plenty of talk and listen time, voice-activated Siri access",
+        category : "Man" ,
+
+    },
+    {
+        id : 10 ,
+        img : "https://pitshirts.in/cdn/shop/files/InShot-20241209_155704713.jpg?v=1755319102&width=1946" ,
+        product : "airpord air",
+        price : "199$",
+        text : "With plenty of talk and listen time, voice-activated Siri access",
+        category : "Man" ,
+
+    },
+    {
+        id : 11 ,
+        img : "https://i.etsystatic.com/21891580/r/il/e46f30/4093954139/il_1588xN.4093954139_joi1.jpg" ,
+        product : "airpord air",
+        price : "199$",
+        text : "With plenty of talk and listen time, voice-activated Siri access",
+        category : "Kid" ,
+
+    },
+    {
+        id : 12 ,
+        img : "https://i.etsystatic.com/21891580/r/il/7108fe/4083553949/il_1588xN.4083553949_tqhq.jpg" ,
+        product : "airpord air",
+        price : "199$",
+        text : "With plenty of talk and listen time, voice-activated Siri access",
+        category : "Kid" ,
+
+    },
+    {
+        id : 13 ,
+        img : "https://i.etsystatic.com/21891580/r/il/01d7c0/4108945039/il_1588xN.4108945039_l4pr.jpg" ,
+        product : "airpord air",
+        price : "199$",
+        text : "With plenty of talk and listen time, voice-activated Siri access",
+        category : "Kid" ,
+
+    },
+    {
+        id : 14 ,
+        img : "https://m.media-amazon.com/images/I/51ce76AJ-WL.jpg" ,
+        product : "airpord air",
+        price : "199$",
+        text : "With plenty of talk and listen time, voice-activated Siri access",
+        category : "Kid" ,
+
+    },
+    {
+        id : 15 ,
+       img : "https://shop.vogue.co.uk/cdn/shop/products/Vogue_T-Shirt_black_1.jpg?v=1684164309&width=1080" ,
+        product : "airpord air",
+        price : "199$",
+        text : "With plenty of talk and listen time, voice-activated Siri access",
+        category : "Kid" ,
+    }
+    
+]
+export { productData };
+
---
+++ src/script/Woman.js
@@ -0,0 +1,44 @@
+import { productData } from '../product/cardData.js';
+
+const container = document.getElementById("container");
+
+
+function RenderCard(){
+    let card = ``;
+     const filteredProducts = productData.filter(p => p.category === 'Woman');
+    filteredProducts.map( c => {
+        card += `
+         <div  class="relative flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-fit ">
+            <div class="relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white bg-clip-border rounded-xl h-[20rem] max-sm:m-0.5 max-sm:h-[10.5rem] sm:h-[15rem] md:h-[15rem] lg:h-[15rem] xl:h-[15rem]">
+                <img
+                    src="${c.img}"
+                    alt="${c.category}" class="object-cover w-full h-full" />
+                    
+            </div>
+            <div class="max-sm:p-2 sm:p-3 xl:p-6 ">
+                <div class="flex items-center justify-between mb-2">
+                    <p class="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
+                       ${c.product}
+                    </p>
+                    <p class="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
+                        ${c.price}
+                    </p>
+                </div>
+                <p class="block font-sans text-sm antialiased font-normal leading-normal text-gray-700 opacity-75">
+                     ${c.text}
+                </p>
+            </div>
+            <div class="p-6 pt-0">
+                <button
+                    class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg shadow-gray-900/10 hover:shadow-gray-900/20 focus:opacity-[0.85] active:opacity-[0.85] active:shadow-none block w-full bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
+                    type="button">
+                    Add to Cart
+                </button>
+            </div>
+            </div>
+
+        `
+    });
+    container.innerHTML = card ;
+}
+RenderCard();
